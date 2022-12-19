f2a00e75e115beace794ed9df8516cf81a33d38c
iperezsalameroh <ivan_psh@hotmail.com>
2022-12-19 16:57:18 +0100

    Update

---
+++ src/main/java/Punto/Main.java
@@ -0,0 +1,15 @@
+package Punto;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Punto punto1 = new Punto();
+        Punto punto2 = new Punto(1,1);
+
+        Rectangulo rectangulo = new Rectangulo(punto1,2,3);
+        rectangulo.moveUp(1);
+
+        System.out.println(rectangulo);
+
+    }
+}
---
+++ src/main/java/Punto/Rectangulo.java
@@ -0,0 +1,54 @@
+package Punto;
+
+public class Rectangulo {
+
+
+    private Punto inferiorIzquierda;
+    private Punto inferiorDerecha;
+    private Punto superiorIzquierda;
+    private Punto superiorDerecha;
+
+    public Rectangulo(Punto inferiorIzquierda,Punto superiorDerecha){
+        this.inferiorIzquierda = inferiorIzquierda;
+        this.superiorDerecha= superiorDerecha;
+        this.superiorIzquierda = new Punto(inferiorIzquierda.getX(),superiorDerecha.getY());
+        this.inferiorDerecha = new Punto(superiorDerecha.getX(),inferiorIzquierda.getY());
+    }
+
+    public Rectangulo(Punto inferiorIzquierda, int base,int altura){
+        this.inferiorIzquierda = inferiorIzquierda;
+        this.inferiorDerecha = inferiorIzquierda.getRight(base);
+        this.superiorDerecha = inferiorDerecha.getUp(altura);
+        this.superiorIzquierda = inferiorIzquierda.getUp(altura);
+    }
+
+    public void moveUp(int distance){
+        inferiorIzquierda.moveUp(distance);
+        inferiorDerecha.moveUp(distance);
+        superiorDerecha.moveUp(distance);
+        superiorIzquierda.moveUp(distance);
+    }
+
+    public void moveDown(int distance){
+        moveUp(-distance);
+    }
+    public int getBase(){
+        return (int)(inferiorIzquierda.getDistance(inferiorDerecha));
+    }
+    public int getHight(){
+        return (int)(inferiorDerecha.getDistance(superiorDerecha));
+    }
+    public int perimeter(){
+        return 2*(getBase()+getHight());
+    }
+
+    public int area(){
+        return getBase()*getHight();
+    }
+
+    @Override
+    public String toString(){
+        return inferiorIzquierda +","+ superiorIzquierda +","+ superiorDerecha +","+ inferiorDerecha;
+    }
+
+}
