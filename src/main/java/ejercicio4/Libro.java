d762889164bf35868d370e2acf2fc60661a38bf7
iperezsalameroh <ivan_psh@hotmail.com>
2022-12-19 19:33:00 +0100

    Update

--- src/main/java/ejercicio4/Libro.java
+++ src/main/java/ejercicio4/Libro.java
@@ -2,6 +2,50 @@ package ejercicio4;
 
 public class Libro {
 
-    private
+    private String autor;
+    private String  titulo;
+    private static int cantidadInicial = 5;
+
+    private int cantidadActual;
+
+    private int cantidad;
+
+    public Libro(String autor, String titulo){
+        this.autor = autor;
+        this.titulo = titulo;
+        this.cantidadActual = cantidadInicial;
+    }
+
+    public boolean prestamo(int cantidad){
+        if(cantidad<=cantidadInicial && cantidad>0 && cantidadActual>0) {
+            cantidadActual -= cantidad;
+            return true;
+        }
+        else {
+            System.out.println("No podemos prestar tantos libros");
+            return false;
+        }
+    }
+
+    public boolean devolucion(int cantidad){
+        if(cantidad<=cantidadInicial && cantidadActual<cantidadInicial && cantidad>0) {
+            cantidadActual += cantidad;
+            return true;
+        }else{
+            System.out.println("Ya tenemos todos los libros. Gracias.");
+            return false;
+        }
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    @Override
+    public String toString(){
+        return "Autor: " + autor + "\n" +
+                "Título: " + titulo + "\n" +
+                "Libros disponibles: " + cantidadActual;
+    }
 
 }
--- src/main/java/ejercicio4/Main.java
+++ src/main/java/ejercicio4/Main.java
@@ -3,5 +3,15 @@ package ejercicio4;
 public class Main {
     public static void main(String[] args) {
 
+        Libro  libro = new Libro("Pepe", "Las vidas de Bonifacio");
+
+        System.out.println(libro);
+
+        libro.prestamo(3);
+        System.out.println(libro.getCantidadActual());
+        libro.devolucion(4);
+
+        System.out.println(libro);
+
     }
 }
