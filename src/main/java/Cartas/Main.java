3732fc3ed53a4608748b47f4e1f64f86d8e253a6
Ivan Perez <ivan_psh@hotmail.com>
2022-12-20 09:37:18 +0100

    Initial commit

--- src/main/java/Cartas/Main.java
+++ src/main/java/Cartas/Main.java
@@ -3,7 +3,7 @@ package Cartas;
 //barajar cartas
 //coger carta arriba
 //coger carta abajo
-//mostrar última cartaxd
+//mostrar última carta
 
 public class Main {
     public static void main(String[] args) {
