dbebbb4be97b4062647a509f29ce2aa6f10a70f2
Ivan Perez <ivan_psh@hotmail.com>
2022-12-21 10:41:23 +0100

    Update

--- src/main/java/Ejercicio6/Fecha.java
+++ src/main/java/Ejercicio6/Fecha.java
@@ -1,31 +1,31 @@
-//package Ejercicio6;
-//
-//public class Fecha {
-//
-//    private int dia;
-//    private int mes;
-//    private int anyo;
-//
-//    public Fecha (int dia, int mes, int anyo){
-//
-//        this.dia = dia;
-//        this.mes = mes;
-//        this.anyo = anyo;
-//
-//    }
-//
-//    public Fecha getFecha(){
-//
-//        return ;
-//
-//    }
-//
-//
-//    @Override
-//    public String toString(){
-//
-//        return "Fecha:" + dia + "/" + mes + "/" + anyo;
-//
-//    }
-//
-//}
+package Ejercicio6;
+
+public class Fecha {
+
+    private int dia;
+    private int mes;
+    private int anyo;
+
+    public Fecha (int dia, int mes, int anyo){
+
+        this.dia = dia;
+        this.mes = mes;
+        this.anyo = anyo;
+
+    }
+
+    public Fecha getFecha(){
+
+        return ;
+
+    }
+
+
+    @Override
+    public String toString(){
+
+        return "Fecha:" + dia + "/" + mes + "/" + anyo;
+
+    }
+
+}
