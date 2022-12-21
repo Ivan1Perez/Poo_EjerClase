36a36563fee4ea4b05fab94ed1c2a586d47f2bac
iperezsalameroh <ivan_psh@hotmail.com>
2022-12-21 20:57:06 +0100

    Update

--- src/main/java/Ejercicio6/Fecha.java
+++ src/main/java/Ejercicio6/Fecha.java
@@ -1,31 +1,64 @@
-//package Ejercicio6;
-//
-//public class Fecha {
-//
-//    private int dia;
-//    private int mes;
-//    private int anyo;
-//
-//    public Fecha (int dia, int mes, int anyo){
-//
-//        this.dia = dia;
-//        this.mes = mes;
-//        this.anyo = anyo;
-//
-//    }
-//
-//    public Fecha getFecha(){
-//
-//        return ;
-//
-//    }
-//
-//
-//    @Override
-//    public String toString(){
-//
-//        return "Fecha:" + dia + "/" + mes + "/" + anyo;
-//
-//    }
-//
-//}
+package Ejercicio6;
+
+public class Fecha {
+
+    private int dia;
+    private int anyo;
+    private Mes mes;
+
+    public Fecha(){
+//        mes = new Mes[Mes.values().length];
+    }
+    public Fecha (int dia, Mes mes, int anyo){
+
+        this.dia = dia;
+        this.mes = mes;
+        this.anyo = anyo;
+
+    }
+
+    public int getDia(){
+        return dia;
+    }
+
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+
+    public Mes getMes(){
+        return mes;
+    }
+
+    public void setMes(Mes mes) {
+        this.mes = mes;
+    }
+
+    public int getAnyo(){
+        return anyo;
+    }
+
+    public void setAnyo(int anyo) {
+        this.anyo = anyo;
+    }
+
+    public void sumarDia(){
+        if(dia==mes.diasTotales(anyo) && mes.ordinal()!=11) {
+            dia = 1;
+            mes = Mes.values()[mes.ordinal()+1];
+        }else if(dia==mes.diasTotales(anyo)){
+            dia = 1;
+            mes = Mes.ENERO;
+            anyo++;
+        }else{
+            dia++;
+        }
+    }
+
+    @Override
+    public String toString(){
+
+        return "Fecha:" + dia + "/" + mes + "/" + anyo;
+
+    }
+
+}
--- src/main/java/Ejercicio6/Main.java
+++ src/main/java/Ejercicio6/Main.java
@@ -3,9 +3,15 @@ package Ejercicio6;
 public class Main {
     public static void main(String[] args) {
 
-        Fecha fecha = new Fecha(22, 03, 1996);
+        Fecha fecha1 = new Fecha(31, Mes.DICIEMBRE, 2001);
+        Fecha fecha2 = new Fecha();
 
-        System.out.println(fecha);
+
+        System.out.println(fecha1);
+//        System.out.println(fecha2);
+        fecha1.sumarDia();
+
+        System.out.println(fecha1);
 
     }
 }
